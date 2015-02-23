6d8995d880039cf5a82a9454ebe943b2b4493853
Tyler Hogan <tybot@cox.net>
2015-02-23 14:32:37 -0800

    Integrated Sprite class into an engine project

---
+++ ICS-161-Engine/Main.cpp
@@ -0,0 +1,79 @@
+// C++ and SDL header files
+#include <iostream>
+#include <sstream>
+#include <Windows.h>
+#include <SDL.h>
+#include "res_path.h"
+#include "cleanup.h"
+#include "SDL_image.h"
+
+// Custom engine header files
+#include "Sprite/Sprite.h"
+
+// Engine-wide constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+/**
+* Log an SDL error with some error message to the output stream of our choice
+* @param os The output stream to write the message to
+* @param msg The error message to write, format will be msg error: SDL_GetError()
+*/
+void logSDLError(std::ostream &os, const std::string &msg){
+	os << msg << " error: " << SDL_GetError() << std::endl;
+	std::ostringstream errMsg;
+	errMsg << " error: " << SDL_GetError() << std::endl;
+	OutputDebugString(errMsg.str().c_str());
+}
+
+
+int main(int argc, char **argv){
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::ostringstream debugMsg;
+		debugMsg << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		OutputDebugString(debugMsg.str().c_str());
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG){
+		logSDLError(std::cout, "IMG_Init");
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Window *window = SDL_CreateWindow("Sprite Demo", 800, 100, SCREEN_WIDTH,
+		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr){
+		logSDLError(std::cout, "CreateWindow");
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr){
+		logSDLError(std::cout, "CreateRenderer");
+		cleanup(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Event e;
+	bool quit = false;
+	while (!quit){
+		while (SDL_PollEvent(&e)){
+			if (e.type == SDL_QUIT){
+				quit = true;
+			}
+		}
+	}
+
+	cleanup(renderer, window);
+	IMG_Quit();
+	SDL_Quit();
+
+	SDL_Quit();
+
+	return 0;
+}
