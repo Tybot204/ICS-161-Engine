b7b3d328b3b4102f0fd447212c2b1a6bbf6233e3
Tyler Hogan <tybot@cox.net>
2015-03-02 19:21:12 -0800

    JSON reader working

--- ICS-161-Engine/Main.cpp
+++ ICS-161-Engine/Main.cpp
@@ -6,10 +6,12 @@
 #include "res_path.h"
 #include "cleanup.h"
 #include "SDL_image.h"
+#include <string>
+#include <fstream>
 
 // Custom engine header files
 #include "Sprite/Sprite.h"
-#include "Json/include/json/json.h"
+#include "Json/json.h"
 
 // Engine-wide constants
 const int SCREEN_WIDTH = 640;
@@ -44,15 +46,14 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
-	SDL_Window *window = SDL_CreateWindow("Sprite Demo", 800, 100, SCREEN_WIDTH,
+	SDL_Window *window = SDL_CreateWindow("ICS 161 Game Engine", 800, 100, SCREEN_WIDTH,
 		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr){
 		logSDLError(std::cout, "CreateWindow");
 		SDL_Quit();
 		return 1;
 	}
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
-		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (renderer == nullptr){
 		logSDLError(std::cout, "CreateRenderer");
 		cleanup(window);
@@ -60,13 +61,26 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
+
+	std::ifstream ifs("test.json");
+	std::string json((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+	std::cout << json << std::endl;
+	Json::Value root;
+	Json::Reader reader;
+	bool parsed = reader.parse(json.c_str(), root, false);
+	if (!parsed){
+		std::cout << "Failed to load level" << std::endl;
+		return 1;
+	}
+	std::string test = root["level"].asString();
+	std::cout << "Root: " << test << std::endl;
+
 	SDL_Event e;
 	bool quit = false;
 	while (!quit){
 		while (SDL_PollEvent(&e)){
-			if (e.type == SDL_QUIT){
+			if (e.type == SDL_QUIT)
 				quit = true;
-			}
 		}
 	}
 
