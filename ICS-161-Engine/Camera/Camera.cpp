aa182a28a6a79dcdf80ba1052d961171e14580a5
rgammad@uci.edu <rgammad@uci.edu>
2015-03-10 10:29:31 -0700

    Camera.cpp

--- ICS-161-Engine/Camera/Camera.cpp
+++ ICS-161-Engine/Camera/Camera.cpp
@@ -3,6 +3,7 @@
 
 Camera::Camera(SDL_Rect rect){
 	this->camera = rect;
+
 }
 
 Camera::~Camera(){};
@@ -21,8 +22,10 @@ int Camera::getX(){
 	return camera.x;
 }
 int Camera::getY(){
-	return camera.y;
+	return camera.x;
 }
+
+
 
 //sets coordinates of camera at x and y
 //@param x an int
@@ -31,6 +34,27 @@ void Camera::setPos(int x, int y){
 	camera.x = x;
 	camera.y = y;
 }
+
+//scrolls through a level by x
+//@param incr how many pixels to move each call
+//@param levelW width of the level
+//@param screenW width of the screen
+void Camera::scrollX(int incr, int levelW, int screenW){
+	if (camera.x < levelW - screenW){
+		camera.x += incr;
+	}
+}
+//scrolls through a level by y
+//@param incr how many pixels to move each call
+//@param levelH height of the level
+//@param screenH height of the screen
+void Camera::scrollY(int incr, int levelH, int screenH){
+	if (camera.y < levelH - screenH){
+		camera.y += incr;
+	}
+}
+
+
 
 //center camera over some object and keep from going outside the level
 void Camera::fixCameraPosition(int levelwidth, int levelheight){
